ce9322025522841a4d1c6e64a6e69f0720556281
Catia Monteiro <monteiro.cmonteiro@icloud.com>
2025-06-27 11:53:34 +0200

    feat: change UI of not found page

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,16 +1,24 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="max-w-2xl mx-auto">
       <div className="text-center">
-        <h2 className="text-2xl font-bold text-gray-900 mb-4">Page Not Found</h2>
-        <p className="text-gray-600 mb-6">Could not find the requested resource.</p>
+        <h2 className="text-2xl font-bold text-foreground">
+          Uh-oh... 
+        </h2>
+        <h2 className="text-2xl font-bold text-foreground mb-4">
+    Something's Offbeat!
+        </h2>
+        <p className="text-foreground/70 mb-8">
+          This page could not be found. But don't worry, your pup's data is
+          still in rhythm.
+        </p>
         <Link
           href="/"
-          className="bg-indigo-600 text-white px-6 py-3 rounded-lg font-medium hover:bg-indigo-700 transition-colors"
+          className="bg-primary text-navbar-items-bg px-6 py-3 shadow-md rounded-lg font-medium hover:bg-primary/80 transition-colors"
         >
-          Return Home
+          Return to Home
         </Link>
       </div>
     </div>
