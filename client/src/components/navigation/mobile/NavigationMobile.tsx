b9cd13e8743fb63c45958c7160be15f459150f08
Catia Monteiro <monteiro.cmonteiro@icloud.com>
2025-09-19 16:26:25 +0200

    fix(ui): limit max-w in bottom nav bar in mobile

--- client/src/components/navigation/mobile/NavigationMobile.tsx
+++ client/src/components/navigation/mobile/NavigationMobile.tsx
@@ -12,7 +12,6 @@ import { getNavItems } from "../NavItems";
 import { useAppContext } from "@/context/Context";
 import { logoutUser } from "@/api/userApi";
 import { toast } from "react-toastify";
-import LoadingSpinner from "@/app/loading";
 
 export default function NavigationMobile() {
   const pathname = usePathname();
@@ -64,73 +63,74 @@ export default function NavigationMobile() {
       {/* Bottom Tab Navigation */}
       <div className="fixed bottom-0 left-0 right-0 bg-navbar-bg border-t border-primary/20 z-50 lg:hidden">
         {/* Navigation Tabs */}
-        <nav className="flex items-center justify-center px-2">
-          {navItems.map((item) => {
-            const isActive = pathname === item.href;
+        <div className="w-full max-w-md mx-auto">
+          <nav className="flex items-center justify-center px-2 max-w-lg">
+            {navItems.map((item) => {
+              const isActive = pathname === item.href;
 
-            // Handle logout
-            if (item.href === "/logout") {
+              // Handle logout
+              if (item.href === "/logout") {
+                return (
+                  <button
+                    key={item.label}
+                    onClick={handleLogout}
+                    disabled={userState.isLoading}
+                    className="flex flex-col items-center justify-center px-1 py-2 rounded-lg transition-all duration-200 min-w-0 flex-1"
+                  >
+                    {/* Icon */}
+                    <div className="flex items-center justify-center w-7 h-7 rounded-lg transition-all duration-200 mb-1 text-foreground/60 bg-transparent">
+                      {item.icon}
+                    </div>
+
+                    {/* Label */}
+                    <span className="text-xs text-center font-bold transition-all duration-200 text-foreground/60">
+                      {userState.isLoading ? (
+                        <div className="flex items-center gap-2">
+                          Signing out...
+                        </div>
+                      ) : (
+                        item.label
+                      )}
+                    </span>
+                  </button>
+                );
+              }
+
+              // Handle remaining icons
               return (
-                <button
+                <Link
                   key={item.label}
-                  onClick={handleLogout}
-                  disabled={userState.isLoading}
+                  href={item.href}
                   className="flex flex-col items-center justify-center px-3 py-2 rounded-lg transition-all duration-200 min-w-0 flex-1"
                 >
                   {/* Icon */}
-                  <div className="flex items-center justify-center w-7 h-7 rounded-lg transition-all duration-200 mb-1 text-foreground/60 bg-transparent">
+                  <div
+                    className={`
+                    flex items-center justify-center w-7 h-7 rounded-lg transition-all duration-200 mb-1
+                    ${
+                      isActive
+                        ? "bg-primary text-navbar-bg shadow-md p-0.5"
+                        : "text-foreground/60 bg-transparent"
+                    }
+                  `}
+                  >
                     {item.icon}
                   </div>
 
                   {/* Label */}
-                  <span className="text-xs text-center font-bold transition-all duration-200 text-foreground/60">
-                    {userState.isLoading ? (
-                      <div className="flex items-center gap-2">
-                        {/* <div className="w-3 h-3 border-2 border-current border-t-transparent rounded-full animate-spin"></div> */}
-                        <LoadingSpinner size="sm" color="white" />
-                        Signing out...
-                      </div>
-                    ) : (
-                      item.label
-                    )}
-                  </span>
-                </button>
-              );
-            }
-
-            return (
-              <Link
-                key={item.label}
-                href={item.href}
-                className="flex flex-col items-center justify-center px-3 py-2 rounded-lg transition-all duration-200 min-w-0 flex-1"
-              >
-                {/* Icon */}
-                <div
-                  className={`
-                    flex items-center justify-center w-7 h-7 rounded-lg transition-all duration-200 mb-1
-                    ${
-                      isActive
-                        ? "bg-primary text-navbar-bg shadow-md"
-                        : "text-foreground/60 bg-transparent"
-                    }
-                  `}
-                >
-                  {item.icon}
-                </div>
-
-                {/* Label */}
-                <span
-                  className={`
+                  <span
+                    className={`
                     text-xs text-center font-bold transition-all duration-200
                     ${isActive ? "text-primary" : "text-foreground/60"}
                   `}
-                >
-                  {item.label}
-                </span>
-              </Link>
-            );
-          })}
-        </nav>
+                  >
+                    {item.label}
+                  </span>
+                </Link>
+              );
+            })}
+          </nav>
+        </div>
       </div>
     </>
   );
