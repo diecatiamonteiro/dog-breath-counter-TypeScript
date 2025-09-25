de3f7c90247355a82942f3050c1a86cd4b1edd6c
Catia Monteiro <monteiro.cmonteiro@icloud.com>
2025-09-25 13:32:28 +0200

    feat(ui): update NotFound page with Button component and improve page UI

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,23 +1,19 @@
-import Link from "next/link";
+import Button from "@/components/Button";
 
 export default function NotFound() {
   return (
-    <div className="max-w-2xl mx-auto">
-      <div className="text-center">
-        <h2 className="text-2xl font-bold text-foreground">Uh-oh...</h2>
-        <h2 className="text-2xl font-bold text-foreground mb-4">
-          Something’s Offbeat…
+    <div className="max-w-2xl">
+      <div className="flex flex-col">
+        <h2 className="text-lg md:text-2xl font-bold text-foreground mb-3">
+          Uh-oh... Something’s Offbeat!
         </h2>
-        <p className="text-foreground/70 mb-8">
+        <p className="text-foreground/70 leading-tight">
           This page could not be found. But don’t worry, your pup’s data is
           still in rhythm.
         </p>
-        <Link
-          href="/"
-          className="bg-primary text-main-text-bg px-6 py-3 shadow-md rounded-lg font-medium hover:bg-primary/80 transition-colors"
-        >
+        <Button href="/" variant="primary" className="w-fit mt-8">
           Return to Home
-        </Link>
+        </Button>
       </div>
     </div>
   );
